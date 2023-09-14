6ac8c3b0a7b24538d0e1929c07746022cdafc52b
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2023-09-14 13:35:37 +0300

    exercise1-fix.sh

--- exercise1-fix.sh
+++ exercise1-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get install iptables-persistent
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables-save > /etc/iptables/rules.v4
 sudo iptables-restore < /etc/iptables/rules.v4
