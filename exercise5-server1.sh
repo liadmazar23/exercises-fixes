deecd6f37531ea7a24d7b15173203376060ce2a2
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2023-09-14 16:37:14 +0300

    exercise5-server1.sh

--- exercise5-server1.sh
+++ exercise5-server1.sh
@@ -1,3 +1,16 @@
-!# bin/bash
-ssh-keygen -t ed25519
-ssh-copy-id -i id_ed25519.pub vagrant@192.168.60.11
+#!/bin/bash
+
+sudo apt-get install sshpass -y
+
+ssh-keygen -t rsa -N '' -f /home/vagrant/.ssh/id_rsa <<< y
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa
+chown vagrant:vagrant /home/vagrant/.ssh/id_rsa.pub
+
+chmod 764 /home/vagrant/.ssh/id_rsa.pub
+
+echo 'sshpass -p vagrant ssh-copy-id -o "StrictHostKeyChecking=no" -i /home/vagrant/.ssh/id_rsa.pub vagrant@192.168.60.11' > /home/vagrant/sshing.sh
+
+chown vagrant:vagrant /home/vagrant/sshing.sh
+chmod 774 /home/vagrant/sshing.sh
+
+sudo sed -i '$a\    StrictHostKeyChecking no' /etc/ssh/ssh_config
