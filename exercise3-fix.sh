52b595d0f1b0bbe89db45187bbb5a52034875839
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2023-09-14 10:25:17 +0300

    exercise3-fix.sh

--- exercise3-fix.sh
+++ exercise3-fix.sh
@@ -1,5 +1,5 @@
 !# bin/bash	
-
+sudo chmod 777 /etc/apache2/sites-available/000-default.conf
 sudo vim /etc/apache2/sites-available/000-default.conf
 echo "        <Location "/">
           Require all granted
