2c5b74b590a652750042de1ae427e35d0ace5044
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2023-09-14 16:38:48 +0300

    exercise1-fix.sh

--- exercise1-fix.sh
+++ exercise1-fix.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo apt-get install iptables-persistent
-sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-sudo iptables-save > /etc/iptables/rules.v4
-sudo iptables-restore < /etc/iptables/rules.v4
-
+sudo ip route del 208.86.224.90
